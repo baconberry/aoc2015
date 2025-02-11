81230a39251fe0aa59ad7338d6cdd914c6297102
Heli Villarreal <heli.v@pm.me>
2025-02-11 21:01:01 +0100

    Day 20 solved (divisor sqrt optimization)

--- src/main/java/org/baconberry/aoc2015/day/Twenty.java
+++ src/main/java/org/baconberry/aoc2015/day/Twenty.java
@@ -3,7 +3,6 @@ package org.baconberry.aoc2015.day;
 import org.baconberry.aoc2015.ISolver;
 
 import java.util.List;
-import java.util.concurrent.atomic.LongAdder;
 
 public class Twenty implements ISolver {
     int multiplier = 10;
@@ -22,14 +21,20 @@ public class Twenty implements ISolver {
         return String.valueOf(n);
     }
 
+    // naive 110,62s user 0,27s system 99% cpu 1:50,99 total
+    // optimized 0,40s user 0,04s system 102% cpu 0,436 total
     int calculatePresents(int houseNumber, int part) {
-        int sum = houseNumber * multiplier;
-        for (int i = 1; i <= houseNumber / 2; i++) {
+        int sum = 0;
+        int sqr = (int) Math.sqrt(houseNumber);
+        for (int i = 1; i <= sqr; i++) {
             if (houseNumber % i == 0) {
-                if (part == 2 && houseNumber / i > 50) {
-                    continue;
+                int j = houseNumber / i;
+                if (part == 1 || (part == 2 && j <= 50)) {
+                    sum += i * multiplier;
                 }
-                sum += i * multiplier;
+                if (j != sqr && (part == 1 || (part == 2 && i <= 50))) {
+                    sum += j * multiplier;
+                }
             }
         }
         return sum;
