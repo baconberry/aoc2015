6377d9c683a24766be7e609df15d78a9d29c4c5e
Heli Villarreal <heli.v@pm.me>
2025-02-11 20:13:35 +0100

    Day 20 part 2 solved (bruteforce, needs better solution)

--- src/main/java/org/baconberry/aoc2015/day/Twenty.java
+++ src/main/java/org/baconberry/aoc2015/day/Twenty.java
@@ -3,24 +3,33 @@ package org.baconberry.aoc2015.day;
 import org.baconberry.aoc2015.ISolver;
 
 import java.util.List;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Twenty implements ISolver {
+    int multiplier = 10;
+
     @Override
     public String solve(List<String> lines, int part) {
         int minPresents = Integer.parseInt(lines.getFirst());
         int c = 0;
         int n = 0;
-        while(c<minPresents){
-            c = calculatePresents(++n);
+        if (part == 2) {
+            multiplier = 11;
+        }
+        while (c < minPresents) {
+            c = calculatePresents(++n, part);
         }
         return String.valueOf(n);
     }
 
-    int calculatePresents(int houseNumber){
-        int sum = houseNumber*10;
-        for (int i = 1; i <= houseNumber/2; i++) {
-            if(houseNumber%i==0){
-                sum += i*10;
+    int calculatePresents(int houseNumber, int part) {
+        int sum = houseNumber * multiplier;
+        for (int i = 1; i <= houseNumber / 2; i++) {
+            if (houseNumber % i == 0) {
+                if (part == 2 && houseNumber / i > 50) {
+                    continue;
+                }
+                sum += i * multiplier;
             }
         }
         return sum;
