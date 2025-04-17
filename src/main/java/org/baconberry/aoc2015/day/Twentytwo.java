f0d6d5b2b65107e28749f8b2fd908dca60702778
Heli Villarreal <heli.v@pm.me>
2025-04-17 11:00:34 +0200

    Refactored day 22 part 1

--- src/main/java/org/baconberry/aoc2015/day/Twentytwo.java
+++ src/main/java/org/baconberry/aoc2015/day/Twentytwo.java
@@ -1,7 +1,6 @@
 package org.baconberry.aoc2015.day;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.ToString;
 import lombok.With;
 import lombok.experimental.SuperBuilder;
@@ -14,7 +13,7 @@ import java.util.List;
 
 @Slf4j
 public class Twentytwo implements ISolver {
-    List<Effect> effects = createEffects();
+    List<IEffect> effects = createEffects();
     int globalMin = Integer.MAX_VALUE;
 
     int startTurnHandicap = 0;
@@ -24,36 +23,36 @@ public class Twentytwo implements ISolver {
     public String solve(List<String> lines, int part) {
         var player = new Player(50, 500, 0, 0, List.of());
         var boss = new Player(51, 0, 9, 0, List.of());
-        if(part==2){
+        if (part == 2) {
             startTurnHandicap = 1;
         }
 
-        var min = calculateMinManaCost(player, boss, 0,  List.of());
+        var min = playerTurn(player, boss, 0, List.of());
 
         return String.valueOf(min);
     }
 
-    private int calculateMinManaCost(Player player, Player boss, int manaCost, List<Effect> playerEffects) {
-        player = player.withHp(player.hp - startTurnHandicap);
-        if (player.hp() <= 0 || manaCost >= player.mp || globalMin <= manaCost) {
+    private int playerTurn(Player player, Player boss, int manaCost, List<IEffect> srcplayerEffects) {
+        player = player.takeDamage(startTurnHandicap);
+        if (player.isDead() || globalMin < manaCost) {
             return Integer.MAX_VALUE;
         }
-        if (boss.hp() <= 0) {
+        var playerEffects = clone(srcplayerEffects);
+        final var players = applyEffects(player, boss, playerEffects);
+        if (players.getRight().isDead()) {
             return manaCost;
         }
-        var min = Integer.MAX_VALUE;
-
-        for (Effect effect : effects) {
+        int min = Integer.MAX_VALUE;
+        for (IEffect effect : effects) {
             var effectClone = effect.cloneEffect();
-            var effectList = cow(playerEffects, effectClone);
-            var localPlayers = Pair.of(player, boss);
-            for (Effect eff : effectList) {
-                localPlayers = eff.apply(localPlayers.getLeft(), localPlayers.getRight());
+            int manaUsed = manaCost + effectClone.cost();
+            if (manaUsed > player.mp() || effectInList(effectClone, playerEffects)) {
+                continue;
             }
-            var playerClone = localPlayers.getLeft();
-            int bossDamage = Math.max(1, boss.damage - player.armor());
-            playerClone = playerClone.withHp(playerClone.hp - bossDamage);
-            var localResult = calculateMinManaCost(playerClone, localPlayers.getRight(), manaCost + effectClone.cost, effectList);
+
+            var localPlayers = effectClone.apply(players.getLeft(), players.getRight());
+            var effectList = cow(playerEffects, effectClone);
+            int localResult = bossTurn(localPlayers.getLeft(), localPlayers.getRight(), manaUsed, effectList);
             if (localResult < min) {
                 min = localResult;
             }
@@ -61,30 +60,84 @@ public class Twentytwo implements ISolver {
                 globalMin = localResult;
             }
         }
+
         return min;
     }
 
-    private List<Effect> cow(List<Effect> pEffects, Effect toAdd) {
-        var list = new ArrayList<Effect>(pEffects.size() + 1);
-        for (Effect effect : pEffects) {
-            if (effect.turns <= 0) {
-                continue;
-            }
-            list.add(effect.cloneEffect());
+    private List<IEffect> clone(List<IEffect> playerEffects) {
+        return playerEffects.stream()
+                .map(IEffect::cloneEffect)
+                .toList();
+    }
+
+    private Pair<Player, Player> applyEffects(Player player, Player boss, List<IEffect> playerEffects) {
+        var players = Pair.of(player, boss);
+        for (IEffect effect : playerEffects) {
+            players = effect.apply(players.getLeft(), players.getRight());
         }
+        return players;
+    }
+
+    private int bossTurn(Player player, Player boss, int manaCost, List<IEffect> playerEffects) {
+        var players = applyEffects(player, boss, playerEffects);
+        if (players.getRight().isDead()) {
+            return manaCost;
+        }
+        var damagedPlayer = players.getRight().hit(players.getLeft());
+        return playerTurn(damagedPlayer, players.getRight(), manaCost, playerEffects);
+    }
+
+    private boolean effectInList(IEffect effectClone, List<IEffect> effects) {
+        return effects.stream()
+                .filter(IEffect::isActive)
+                .anyMatch(effect -> effect.name().equals(effectClone.name()));
+    }
+
+    private List<IEffect> cow(List<IEffect> pEffects, IEffect toAdd) {
+        var list = new ArrayList<IEffect>(pEffects.size() + 1);
+        list.addAll(clone(pEffects));
         list.add(toAdd);
         return list;
     }
 
     @With
     record Player(int hp, int mp, int damage, int armor, List<Effect> effects) {
+
+
+        boolean isDead() {
+            return hp() <= 0;
+        }
+
+        Player takeDamage(int damage) {
+            if (damage > 0) {
+                return withHp(hp - damage);
+            }
+            return this;
+        }
+
+        Player hit(Player otherPlayer) {
+            int damage = Math.max(1, damage() - otherPlayer.armor());
+            return otherPlayer.takeDamage(damage);
+        }
     }
 
+
+    interface IEffect {
+        Pair<Player, Player> apply(Player player, Player challenger);
+
+        IEffect cloneEffect();
+
+        boolean isActive();
+
+        int cost();
+
+        String name();
+    }
 
     @AllArgsConstructor
     @SuperBuilder(toBuilder = true)
     @ToString(onlyExplicitlyIncluded = true)
-    static class Effect {
+    static class Effect implements IEffect {
         @ToString.Include
         String name;
         int cost;
@@ -96,65 +149,120 @@ public class Twentytwo implements ISolver {
         int turns;
 
 
-        /**
-         * @return true if effect is ended
-         */
-        Pair<Player, Player> apply(Player player, Player challenger) {
-            if (turns-- <= 0) {
+        public Pair<Player, Player> apply(Player player, Player challenger) {
+            if (!isActive()) {
                 return Pair.of(player, challenger);
             }
-            challenger = challenger.withHp(challenger.hp - damage);
-            player = player.withHp(player.hp + heal)
+            turns--;
+            var boss = challenger.takeDamage(damage);
+            var effectedPlayer = player.withHp(player.hp + heal)
                     .withArmor(player.armor + armor)
                     .withMp(player.mp + mana);
-            return Pair.of(player, challenger);
+            return Pair.of(effectedPlayer, boss);
         }
 
-        Effect cloneEffect(){
+        public Effect cloneEffect() {
             return this.toBuilder().build();
+        }
+
+        public boolean isActive() {
+            return turns > 0;
+        }
+
+        @Override
+        public int cost() {
+            return cost;
+        }
+
+        @Override
+        public String name() {
+            return name;
         }
     }
 
     static class Shield extends Effect {
 
-        boolean applied;
+        boolean reverted;
 
         public Shield(String name, int cost, int damage, int heal, int armor, int mana, int turns) {
             super(name, cost, damage, heal, armor, mana, turns);
         }
-        public Shield(String name, int cost, int damage, int heal, int armor, int mana, int turns, boolean applied) {
-            super(name, cost, damage, heal, armor, mana, turns);
-            this.applied = applied;
-        }
 
         @Override
-        Pair<Player, Player> apply(Player player, Player challenger) {
-            if (applied) {
-                armor = 0;
-            }
+        public Pair<Player, Player> apply(Player player, Player challenger) {
             try {
-                if (turns == 0) {
-                    return super.apply(player.withArmor(player.armor() - 7), challenger);
+                if (turns == 1 && !reverted) {
+                    reverted = true;
+                    return super.apply(player.withArmor(player.armor() - armor), challenger);
                 }
                 return super.apply(player, challenger);
             } finally {
-                applied = true;
+                super.armor = 0;
             }
         }
 
         @Override
-        Effect cloneEffect() {
-            return new Shield(name, cost, damage, heal, armor, mana, turns, applied);
+        public Effect cloneEffect() {
+            return new Shield(name, cost, damage, heal, armor, mana, turns);
         }
     }
 
-    private List<Effect> createEffects() {
+    static class DelayedByOne implements IEffect {
+
+        final IEffect delegate;
+        boolean canApply;
+
+        public DelayedByOne(IEffect wrapper) {
+            delegate = wrapper;
+            canApply = false;
+        }
+
+        private DelayedByOne(IEffect wrapper, boolean canApply) {
+            delegate = wrapper;
+            this.canApply = canApply;
+        }
+
+        public Pair<Player, Player> apply(Player player, Player challenger) {
+            if (canApply) {
+                return delegate.apply(player, challenger);
+            }
+            canApply = true;
+            return Pair.of(player, challenger);
+        }
+
+        @Override
+        public IEffect cloneEffect() {
+            return new DelayedByOne(delegate.cloneEffect(), canApply);
+        }
+
+        @Override
+        public boolean isActive() {
+            return delegate.isActive();
+        }
+
+        @Override
+        public int cost() {
+            return delegate.cost();
+        }
+
+        @Override
+        public String name() {
+            return delegate.name();
+        }
+
+        @Override
+        public String toString() {
+            return "Delayed[%s]".formatted(delegate.toString());
+        }
+    }
+
+    private List<IEffect> createEffects() {
         return List.of(
                 new Effect("Magic Missile", 53, 4, 0, 0, 0, 1),
                 new Effect("Drain", 73, 2, 2, 0, 0, 1),
-                new Shield("Shield", 113, 0, 0, 7, 0, 6),
-                new Effect("Poison", 173, 3, 0, 0, 0, 6),
-                new Effect("Recharge", 229, 0, 0, 0, 101, 5)
+                new DelayedByOne(new Shield("Shield", 113, 0, 0, 7, 0, 6)),
+                new DelayedByOne(new Effect("Poison", 173, 3, 0, 0, 0, 6)),
+                new DelayedByOne(new Effect("Recharge", 229, 0, 0, 0, 101, 5))
         );
     }
 }
